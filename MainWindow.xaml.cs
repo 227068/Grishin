b701bf2c9b14190f430d8568dc6b7a3bc1cf6fef
227068 <andrey.grishin111@gmail.com>
2025-02-08 16:22:55 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+
+namespace PrakticheskayaRabota1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(inputTextBox.Text);
+                double result = 0;
+
+                if (shRadioButton.IsChecked == true)
+                {
+                    result = Math.Sinh(x);
+                }
+                else if (x2RadioButton.IsChecked == true)
+                {
+                    result = Math.Pow(x, 2);
+                }
+                else if (exRadioButton.IsChecked == true)
+                {
+                    result = Math.Exp(x);
+                }
+
+                outputTextBox.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите корректное число.");
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+        }
+    }
+}
+
